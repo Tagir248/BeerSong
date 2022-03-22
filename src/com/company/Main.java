88ceab4b27c3ad87b81b802962af2699789b10e0
Tagir248 <87240584+Tagir248@users.noreply.github.com>
2022-03-22 16:55:24 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int beerNum = 99;
+    String word = "бутылок (бутылки)";
+        while (beerNum>0) {
+        word = "бутылок";
+        System.out.println(beerNum + " " + word + " пива на стене ");
+        System.out.println(beerNum + " " + word + " " + " пива.");
+        System.out.println("Возьми одну.");
+        System.out.println("Пусти по кругу.");
+        beerNum = beerNum - 1;
+        if (beerNum>0) {
+            System.out.println(beerNum + " " + word + " пива на стене");
+        }else {
+            System.out.println("нет бутылок пива");
+        }
+        }
+    }
+}
